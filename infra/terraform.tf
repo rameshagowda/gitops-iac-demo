6e5d31ee12806a65f32348628ef68595dac91cc2
Ramesha Gowda <ramesha.gowda@gmail.com>
2023-03-10 13:56:53 -0500

    Update terraform.tf

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create an ACR instance
 resource "azurerm_container_registry" "acr" {
-  name                     = "gitopsdemoacr"
+  name                     = "gitopsdemohelloacr"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   sku                      = "Standard"
@@ -35,7 +35,7 @@ resource "azurerm_container_registry" "acr" {
 
 # Create an AKS cluster
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "gitopsdemoaks"
+  name                = "gitopsdemohelloaks"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix = "myaks"
