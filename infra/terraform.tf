55c76a9dc02bff1431a4ec1f2b2c170037fce7cb
Ramesha Gowda <ramesha.gowda@gmail.com>
2023-03-10 08:45:59 -0500

    Create terraform.tf

---
+++ infra/terraform.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.3"
+    }
+  }
+
+ # backend "azurerm" {
+ #   resource_group_name  = "terraform-state-rg"
+ #   storage_account_name = "terraformstate1234"
+ #   container_name       = "terraform-state-container"
+ #   key                  = "aks-cluster.tfstate"
+ # }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "devopsdemo-rg"
+  location = "eastus"
+}
+
+# Create an ACR instance
+resource "azurerm_container_registry" "acr" {
+  name                     = "devopsdemo-acr"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku                      = "Standard"
+  admin_enabled            = false
+}
+
+# Create an AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "devopsdemo-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix = "myaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+# Assign AcrImageSigner role to AKS cluster identity for ACR instance scope
+resource "azurerm_role_assignment" "acr_signer" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrImageSigner"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
